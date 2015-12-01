ee8688f7c969904d74afe9cdc81a24a1f9ab1162
shuhei kinukawa <kinushu@gmail.com>
2015-12-01 21:07:22 +0900

    リンク調整

--- lib/site_parser.rb
+++ lib/site_parser.rb
@@ -1,12 +1,37 @@
+require "uri"
 require "open-uri"
 require "nokogiri"
 require 'json'
 
 module SiteParser
 
+  class Link
+    def initialize(href:"", text:"", uri:nil)
+      @conf = {
+        href: href,
+        text: text,
+        uri:  uri,
+      }
+    end
+
+    def href
+      @conf[:href]
+    end
+    def text
+      @conf[:text]
+    end
+    def real_href
+      uri = @conf[:uri]
+      [uri.scheme, "://", uri.host, @conf[:href]].join
+    end
+
+  end
+
   module_function
 
   def parse_url(url)
+
+    uri = URI.parse(url)
 
     charset = nil
     html = open(url) do |f|
@@ -31,9 +56,8 @@ module SiteParser
       href = node[:href].to_s
       next if href.size == 0
 
-      links[href] = {
-        text: node.text,
-      }
+      links[href] = Link.new(href: href, text: node.text, uri: uri)
+
     end
     result[:links] = links.sort.to_h
 
